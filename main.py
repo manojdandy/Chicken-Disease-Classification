04658641a57579cad0094d36de59eda38f753a73
m0k08nh <manoj.kumar16@walmart.com>
2023-06-04 21:01:52 +0530

    added stage 01 pipeline

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeLine
 
+STAGE_NAME = 'Data Ingestion Stage'
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> Stage {STAGE_NAME} Started <<<<<<<<")
+        data_ingestion_training_pipeline = DataIngestionTrainingPipeLine()
+        data_ingestion_training_pipeline.main() 
+        logger.info(f">>>>>> Stage {STAGE_NAME} Completed <<<<<<<<")
+    except Exception as e:
+        raise e   
---
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,29 @@
+
+
+from cnnClassifier.components.data_ingestion import DataIngestion
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier import logger
+
+STAGE_NAME = 'Data Ingestion Stage'
+
+class DataIngestionTrainingPipeLine:
+    def __init__(self) -> None:
+        pass
+    
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+        
+        
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> Stage {STAGE_NAME} Started <<<<<<<<")
+        data_ingestion_training_pipeline = DataIngestionTrainingPipeLine()
+        data_ingestion_training_pipeline.main() 
+        logger.info(f">>>>>> Stage {STAGE_NAME} Completed <<<<<<<<")
+    except Exception as e:
+        raise e      
+        
