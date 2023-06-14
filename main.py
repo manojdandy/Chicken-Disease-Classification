ce2878c229dccf0e319f0741cc1ebe0dfe2acc22
m0k08nh <manoj.kumar16@walmart.com>
2023-06-14 17:28:24 +0530

    evaluation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeLine
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeLine
 from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeLine
+from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
 
 STAGE_NAME = 'Data Ingestion Stage'
 
@@ -38,4 +39,15 @@ if __name__ == '__main__':
         logger.info(f'>>>>>>>> Stage {STAGE_NAME} Completed >>>>>>>>>>>')
     except Exception as e:
         raise e     
-    
+
+
+STAGE_NAME = 'Evaluation Stage'
+
+if __name__ == '__main__':
+    try:
+        logger.info(f'>>>>>>>> Stage {STAGE_NAME} Started >>>>>>>>>>>')
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f'>>>>>>>> Stage {STAGE_NAME} Completed >>>>>>>>>>>')
+    except Exception as e:
+        raise e     
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -2,7 +2,7 @@
 import os
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml,create_directories
-from cnnClassifier.config.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig
+from cnnClassifier.config.config_entity import DataIngestionConfig, EvaluationConfig, PrepareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig
 
 
 class ConfigurationManager:
@@ -88,4 +88,13 @@ class ConfigurationManager:
         )
 
         return training_config
-            
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config        
